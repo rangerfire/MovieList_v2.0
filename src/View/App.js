1054649b40c13bf9b56c4a426fb7ad99eb20a54e
rangerfire <420554242@qq.com>
2020-09-21 16:05:59 -0400

    Jackson/file structure
    Create folders as Jason said, and move files into right place.

---
+++ src/View/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>this is home page</p>
+    </div>
+  );
+}
+
+export default App;
