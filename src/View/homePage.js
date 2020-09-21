9ef89cc676342b64d0591bcb56736dfe5bbd91e5
ldh <donghenl@uci.edu>
2020-09-21 15:05:44 -0700

    nav-bar-and-home-page-raw

---
+++ src/View/homePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ({ onPageChange }) {
+  return (
+    <React.Fragment>
+      <h1>This is the home page</h1>
+    </React.Fragment>
+  );
+}
---
+++ src/View/sideNav.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../Styles/sideNav.css";
+
+export default function ({ onPageChange }) {
+  return (
+    <div className="side-nav">
+      <div className={"link"} onClick={onPageChange.bind(this, 0)}>
+        Home
+      </div>
+      <div className={"link"} onClick={onPageChange.bind(this, 1)}>
+        Movie List
+      </div>
+      <div className={"link"} onClick={onPageChange.bind(this, 2)}>
+        Movie List of Liked
+      </div>
+      <div className={"link"} onClick={onPageChange.bind(this, 3)}>
+        Movie List of Blocked
+      </div>
+    </div>
+  );
+}
