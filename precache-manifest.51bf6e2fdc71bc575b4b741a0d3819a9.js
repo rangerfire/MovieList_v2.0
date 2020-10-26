ae36b2b3241d82075d59df21b2cd649e3bd01377
rangerfire <420554242@qq.com>
2020-10-26 19:02:23 -0400

    Updates

---
+++ precache-manifest.51bf6e2fdc71bc575b4b741a0d3819a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cb0d79d81780187e8e2cf5b8b78b1fa",
+    "url": "/MovieList_v2.0/index.html"
+  },
+  {
+    "revision": "111915fb1a4c18af18a5",
+    "url": "/MovieList_v2.0/static/css/2.373572e1.chunk.css"
+  },
+  {
+    "revision": "212cfc154e9f98d86922",
+    "url": "/MovieList_v2.0/static/css/main.40937ba2.chunk.css"
+  },
+  {
+    "revision": "111915fb1a4c18af18a5",
+    "url": "/MovieList_v2.0/static/js/2.1fea98df.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MovieList_v2.0/static/js/2.1fea98df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "212cfc154e9f98d86922",
+    "url": "/MovieList_v2.0/static/js/main.d6dd730f.chunk.js"
+  },
+  {
+    "revision": "80cd0403773aff1e0a4b",
+    "url": "/MovieList_v2.0/static/js/runtime-main.7003fed5.js"
+  }
+]);
