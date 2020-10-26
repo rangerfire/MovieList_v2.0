4ba485766f7413e6e3e1b33499e7d953e38a4096
rangerfire <420554242@qq.com>
2020-10-26 19:12:45 -0400

    test deploy
    add sth in homepage

--- src/View/homePage.js
+++ src/View/homePage.js
@@ -5,6 +5,9 @@ export default function homePage({ onPageChange }) {
   return (
     <React.Fragment>
       <header>Our Top Rated Movie List</header>
+      <p>111123312321413123</p>
+      <p>111123312321413123</p>
+      <p>111123312321413123</p>
       <Caro />
     </React.Fragment>
   );
